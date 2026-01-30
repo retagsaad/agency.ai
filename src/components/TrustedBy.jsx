fb234d8fa8b1b749c1aa77cf16bf6554fc172821
retag <retagsaad77@gmail.com>
2026-01-30 17:04:57 +0200

    trustedBy

--- src/components/TrustedBy.jsx
+++ src/components/TrustedBy.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import assets from '../assets/assets'
-import { company_logos } from '../assets/assets'
+import {company_logos} from '../assets/assets'
 function TrustedBy() {
   return (
-  <div>
-      <p>Trusted by Leading Companies</p>
-      <div>
-        {company_logos.map((index,logo)=>{
-          <img key={index} src={logo} alt="" />
-        })}
+  <div className='flex flex-col items-center px-4 sm:px-12 lg:px-24 xl:px-40 gap-10 text-gray-700 dark:text-white/80'>
+      <h3 className='font-semibold'>Trusted by Leading Companies</h3>
+      <div className='flex items-center justify-center flex-wrap gap-10 m-4'>
+        {company_logos.map((index , logo) => (
+          <img key={logo} src={index} className='max-h-5 sm:max-h-6 dark:drop-shadow-xl' />
+        ))}
       </div>
     </div>
   )
