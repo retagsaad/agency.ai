957ca609bcba25895045ca39dc09da43ee17117f
retag <retagsaad77@gmail.com>
2026-01-31 22:21:30 +0200

    team page

--- src/components/TeamPage.jsx
+++ src/components/TeamPage.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import assets from '../assets/assets'
-
+import React from "react";
+import assets from "../assets/assets";
+import { teamData } from "../assets/assets";
 function teamPage() {
   return (
-    <div>teamPage</div>
-  )
+    <div
+      id="team"
+      className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-800 dark:text-white"
+    >
+      <div className="relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white">
+        <h1 className="text-3xl sm:text-5xl font-medium">Meet the team</h1>
+        <p className="max-w-lg text-center text-gray-500 dark:text-white/75 mb-6">
+          A passionate team of digital experts dedicated to your brands success.
+        </p>
+        </div>
+        <div className="grid grid-cols-2 ms:grid-cols-3 xl:grid-cols-4 gap-5">
+        {teamData.map(({name,image,title ,index})=>(
+          <div key={index} className="flex max-sm:flex-col items-center gap-5 p-4 rounded-xl border border-gray-100 dark:border-gray-700 bg-white dark:bg-gray-900 shadow-gray-100 dark:shadow-white/5 hover:scale-103 transition-all duration-400">
+            <img src={image} className="w-12 h-12 rounded-full" />
+            <div className="flex-1">
+            <h4 className="font-bold text-sm">{name}</h4>
+            <p className="text-xs opacity-60">{title}</p>
+            </div>
+           
+          </div>
+      ))}
+      </div>
+      
+    </div>
+  );
 }
 
-export default teamPage
+export default teamPage;
