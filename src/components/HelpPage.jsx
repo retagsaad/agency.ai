15693cbfe3035206720d7eb27b9a31ae41931c56
retag <retagsaad77@gmail.com>
2026-01-31 21:08:14 +0200

    update help page

--- src/components/HelpPage.jsx
+++ src/components/HelpPage.jsx
@@ -52,27 +52,24 @@ function HelpPage() {
       > */}
       <div
       // className="flex items-center gap-10 p-8 hover:p-7.5 hover:m-0.5 transition-all rounded-[10px] bg-white dark:bg-gray-900 z-10 relative"
+      // className="pointer-events-none blur-2xl rounded-full bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 w-[300px] h-[300px] absolute z-0 transition-opacity duration-500 mix-blend-lighten opacity-70"
+        // style={{top:positi1on.y - 50, left:position.x - 150}}
       >
-        <div
-        className="flex flex-col md:grid grid-cols-2"
+        <div className="flex flex-col md:grid grid-cols-2" 
         >
           {services.map(({ id, title, description, icon }) => (
             <div
-            key={id}
-            // className="relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10"
-            
-            className="flex items-center gap-10 p-8 hover:p-7.5 hover:m-0.5 transition-all rounded-[10px] bg-white dark:bg-gray-900 z-10 relative"
-            // className="pointer-event-none blur-2xl rounded-fill bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 w-[300px] h-[300px] absolute z-0 transition-opacity duration-500 mix-blend-lighten opacity-70"
+              key={id}
+              // className="relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10"
+              className="flex items-center gap-10 p-8 hover:p-7.5 hover:m-0.5 transition-all rounded-[10px] bg-white dark:bg-gray-900 z-10 relative"
             >
-              <div
-                // className="flex items-center gap-10 p-8 hover:p-7.5 hover:m-0.5 transition-all rounded-[10px] bg-white dark:bg-gray-900 z-10 relative"
-                //  className="relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10"
-                // className="flex items-center gap-10 p-8 hover:p-7.5 hover:m-0.5 transition-all rounded-[10px] bg-white dark:bg-gray-900 z-10 relative"
-              >
+              <div className="bg-gray-100 dark:bg-gray-700 rounded-full">
                 <img
                   src={icon}
                   className="max-w-24 bg-white dark:bg-gray-900 rounded-full m-2"
                 />
+              </div>
+              <div className="flex-1">
                 <h3 className="font-bold text-lg">{title}</h3>
                 <p className="text-sm mt-2">{description}</p>
               </div>
